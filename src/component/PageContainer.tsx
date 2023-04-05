e709273aa8dd589e3388da58db84f7886bb23aec
mattyokan <myokan@quadrex.org>
2023-04-05 13:43:35 -0500

    Initial page styling

--- src/component/PageContainer.tsx
+++ src/component/PageContainer.tsx
@@ -1,6 +1,10 @@
 import {ReactNode} from "react";
 import Head from "next/head";
 import {Inter} from "next/font/google";
+import styles from '@/styles/PageContainer.module.scss';
+import Link from "next/link";
+import {router} from "next/client";
+import {useRouter} from "next/router";
 
 interface PageContainerProps {
     children: ReactNode
@@ -10,15 +14,45 @@ const inter = Inter({subsets: ['latin']})
 
 
 export default function PageContainer(props: PageContainerProps) {
-
+    const router = useRouter();
+    const nav = [
+        {
+            name: "Todos",
+            path: "/todos"
+        },
+        {
+            name: "Done",
+            path: "/done"
+        }
+    ]
     return (
         <>
             <Head>
                 <title>Taskflow</title>
             </Head>
-            <main className={`${inter.className}`}>
-                {props.children}
-            </main>
+            <div className={`${styles.pageContainer} ${inter.className}`}>
+                <div className={styles.navbar}>
+                    <div className={styles.brand}>
+                        <Link href={`/`}>Taskflow</Link>
+                    </div>
+                    <div className={styles.navigation}>
+                        {nav.map((entry) => {
+                            return (<Link className={entry.path === router.pathname ? styles.active : ""} key={entry.path} href={entry.path}>{entry.name}</Link>)
+                        })}
+                    </div>
+                    <div className={styles.search}>
+                        Search
+                    </div>
+                </div>
+                <div className={styles.pageContent}>
+                    <aside className={styles.sidebar}>
+                        Sidebar
+                    </aside>
+                    <main className={styles.main}>
+                        {props.children}
+                    </main>
+                </div>
+            </div>
         </>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import PageContainer from "@/component/PageContainer";
 import {useCodeHooks} from "@/hook/useCodeHooks";
 import {useEffect, useState} from "react";
+import Link from "next/link";
 
 export default function Home() {
     const [api] = useCodeHooks()
@@ -15,7 +16,13 @@ export default function Home() {
     }, [])
     return (
         <PageContainer>
-            {state?.foo ?? "loading..."}
+            Welcome to the application
+
+            <Link href={`/done/lol`}>Done (category lol)</Link>
+            <Link href={`/todo/1`}>TODO View</Link>
+            <Link href={`/todos/`}>TODOs view</Link>
+            <Link href={`/todos/lol`}>TODOs category view</Link>
+            <Link href={`/done`}>Done view</Link>
         </PageContainer>
     )
 }
