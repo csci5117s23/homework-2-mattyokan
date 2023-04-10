c03021106a3bd28f57bbec5e2b52080c9cffa9a0
mattyokan <myokan@quadrex.org>
2023-04-10 12:22:21 -0500

    Minify codehook

--- src/hook/useCodeHooks.tsx
+++ src/hook/useCodeHooks.tsx
@@ -14,51 +14,25 @@ interface CodehooksEndpoints {
 }
 
 export function useCodeHooks(): ApiHook {
-    const baseUrl = process.env.NEXT_PUBLIC_API_ENDPOINT_BASE
-    const apiKey = process.env.NEXT_PUBLIC_API_KEY
-    const {isLoaded, userId, sessionId, getToken} = useAuth();
-
-
-    const fetchAuth = async () => {
-        if (!userId) {
-            return {}
-        } else {
-            const token = await getToken({template: "codehooks"}); // get the token
-            return {
-                'Authorization': 'Bearer ' + token
-            }
-        }
-    }
-
+    const [baseUrl, apiKey] = [process.env.NEXT_PUBLIC_API_ENDPOINT_BASE, process.env.NEXT_PUBLIC_API_KEY]
+    const {isLoaded, userId, getToken} = useAuth();
+    const fetchAuth = async () => userId ? {'Authorization': 'Bearer ' + await getToken({template: "codehooks"})} : {}
     const fullUrl = (url: string): string => `${baseUrl}${url}`
-    const applyProps = async (props: any): Promise<any> => ({
-        headers: {
-            "x-apikey": apiKey
-        },
-        ...props,
-        ...(await fetchAuth())
-    })
-
+    const applyProps = async (props: any): Promise<any> => ({headers: {"x-apikey": apiKey}, ...props, ...(await fetchAuth())})
     return {
         api: {
             makeUrl(url: string): string {
                 return fullUrl(url)
-            },
-            fetchProps(props: any): any {
+            }, fetchProps(props: any): any {
                 return applyProps(props)
-            },
-            fetch(endpoint: string, callback: (res: Response) => void, method: string = "GET"): Promise<void> {
+            }, fetch(endpoint: string, callback: (res: Response) => void, method: string = "GET"): Promise<void> {
                 const fetchCallback = async () => {
-                    console.log("Calling endpoint ", fullUrl(endpoint))
-                    const res = await fetch(fullUrl(endpoint), await applyProps({
-                        method: method
-                    }))
+                    const res = await fetch(fullUrl(endpoint), await applyProps({method: method}))
                     await callback(res)
                 }
 
                 return fetchCallback()
             }
-        },
-        deps: [isLoaded]
+        }, deps: [isLoaded]
     }
 }
